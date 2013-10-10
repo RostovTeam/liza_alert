88b6277819506cf9c920b11c02e44a1a4bb65f1a
Kuvakin Sergey <kuvakin@gmail.com>
2013-10-11 02:22:11 +0400

    ci

--- example_map/example.js
+++ example_map/example.js
@@ -1,35 +1,94 @@
+/*jslint vars: true*/
 /*global google, document, window*/
+
+var addMarkerBtn = document.getElementById('add-marker');
+var addAreaBtn = document.getElementById('add-area');
+var deleteMarkerBtn = document.getElementById('delete-marker');
+var deleteAreaBtn = document.getElementById('delete-area');
+var logArea = document.getElementById('map-log');
+
+function addLog(text) {
+    'use strict';
+    var p = document.createElement('p');
+    p.innerHTML = text;
+    logArea.appendChild(p);
+}
 
 function initialize() {
     'use strict';
     var mapOptions = {
-        zoom: 8,
+        zoom: 12,
         disableDefaultUI: true,
         mapTypeControl: true,
         zoomControl: true,
         scaleControl: true,
-        center: new google.maps.LatLng(-34.397, 150.644),
+        center: new google.maps.LatLng(47.216653, 39.703646),
         mapTypeId: google.maps.MapTypeId.ROADMAP
     };
     var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+    var centerMap = map.getCenter();
+    var markers = [],
+        polygons = [];
+    var marker;
 
-    var marker = new google.maps.Marker({
-        position: map.getCenter(),
-        map: map,
-        title: 'Marker'
-    });
-    google.maps.event.addListener(marker, 'click', function () {
-        map.setZoom(8);
-        map.setCenter(marker.getPosition());
-    });
+    function addMarker(location, type) {
+        marker = new google.maps.Marker({
+            position: location,
+            map: map,
+            draggable: true
+        });
+        addLog('add marker type:' + type);
+        markers.push(marker);
+    }
+
+    function addArea(location, type) {
+        var x = location.lat(), y = location.lng();
+        var bounds = [
+            new google.maps.LatLng(x-0.01, y),
+            new google.maps.LatLng(x-0.01, y-0.01),
+            new google.maps.LatLng(x, y-0.01)
+        ];
+
+        var polygon = new google.maps.Polygon({
+            paths: bounds,
+            strokeColor: '#FF0000',
+            strokeOpacity: 0.8,
+            strokeWeight: 2,
+            fillColor: '#FF0000',
+            fillOpacity: 0.35,
+            editable: true
+        });
+        polygon.setMap(map);
+        addLog('add rectangle type:' + type);
+        polygons.push(polygon);
+    }
+
+    function deleteSelected() {
+
+    }
+
+    addMarkerBtn.onclick = function () {
+        addMarker(centerMap, '');
+    };
+
+    addAreaBtn.onclick = function () {
+        addArea(centerMap, '');
+    };
+
+    deleteMarkerBtn.onclick = function () {
+
+    };
+
+    deleteAreaBtn.onclick = function () {
+
+    };
+
 }
 
-function loadScript() {
+window.onload = function () {
     'use strict';
     var script = document.createElement('script');
     script.type = 'text/javascript';
     script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false&callback=initialize';
     document.body.appendChild(script);
-}
-
-window.onload = loadScript;
+};
