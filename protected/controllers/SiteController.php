cd827ba6ac019fa6067815e36fad8f6dac5090a1
komov.r <komov.r@gmail.com>
2013-10-12 00:00:03 +0400

    clear controller

--- protected/controllers/SiteController.php
+++ protected/controllers/SiteController.php
@@ -8,8 +8,6 @@
 class SiteController extends Controller
 {
 
-    public $defaultAction = 'News';
-
     function actionIndex()
     {
         
@@ -42,74 +40,6 @@ class SiteController extends Controller
         //$role_user = $auth->createRole('user');
         //сохраняем роли и операции
         $auth->save();
-    }
-
-    function actionNews($type = 0, $tag = '', $game = '', $developer = '', $search = '')
-    {
-        $crit = new CDbCriteria();
-
-
-        if ($type != -1)
-            $crit->compare('type', $type);
-
-        $crit->with = array();
-        
-        
-        if ($search)
-        {
-            $crit->compare('title', $search, true);
-            $crit->compare('content', $search, true, 'OR');
-        }
-
-        if ($tag)
-        {
-           $crit->addCondition('id in(select news_id from news_tag nt join tag tg on nt.tag_id=tg.id where tg.name=:tname )');
-           $crit->params+=array(':tname'=>$tag);
-        }
-
-        if ($game)
-        {
-            array_push($crit->with, '_game');
-            $crit->compare('_game.name', $game);
-        }
-
-        if ($developer)
-        {
-            array_push($crit->with, '_developer');
-            $crit->compare('_developer.name', $developer);
-        }
-        
-        //var_dump($crit);
-        $crit->order = 'date_created DESC';
-        $crit->limit = 50;
-
-        $models = News::model()->findAll($crit);
-
-        $this->render('news_list', array('models' => $models));
-    }
-
-    function actionNewsDetail($id)
-    {
-        $model = News::model()->findByPk($id);
-
-        $this->render('news_detail', array('model' => $model));
-    }
-
-    function actionGames()
-    {
-        $model = new Game('search');
-        $model->unsetAttributes();  // clear any default values
-        if (isset($_GET['Game']))
-            $model->attributes = $_GET['Game'];
-
-        $this->render('games_list', array(
-            'model' => $model,
-        ));
-    }
-
-    function actionCompanies()
-    {
-        
     }
 
 }
