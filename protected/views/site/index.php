11b13993f5afbecdbd3ab785d2e9ecd80a172372
Denis Mosolov <denismosolov@gmail.com>
2013-10-12 17:07:30 +0400

    Radius first attempt

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -45,14 +45,58 @@
 </select>
 
 <script type="text/javascript" src="http://yandex.st/jquery/1.10.2/jquery.min.js"></script>
-<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false"></script>
-<script type="text/javascript" src="../static/js/widget.js"></script>
-<script type="text/javascript" src="../static/js/widgetviewmodel.js"></script>
+<script>
+	function initialize() {
+		$.ajax({
+			url: '/api/map/<?php echo $id ?>',
+			//url: 'http://webant.ru/json.php?test1=1',
+			type: 'GET',
+			dataType: 'json',
+			success: function(json) {
+				//var data = json;
+				var data = json.content;
+				var mapOptions = {
+					zoom: 12,
+					//center: new google.maps.LatLng(-25.363882, 131.044922),
+					mapTypeId: google.maps.MapTypeId.ROADMAP
+				};
 
-<script type="text/javascript">
-    function initialize() {
-        $('#map-area').laWidget({route: '/api/map/<?php echo $id ?>'});
-    };
+				var map = new google.maps.Map(document.getElementById('map-canvas'),
+				mapOptions);
 
-    google.maps.event.addDomListener(window, 'load', initialize);
+				var	geocoder = new google.maps.Geocoder();
+				geocoder.geocode({address: data.lost.city.name}, function(results, status) {
+					if (status == google.maps.GeocoderStatus.OK) {
+						map.setCenter(results[0].geometry.location);
+					}
+				});
+
+				// add markers
+				data.ballons.forEach(function(ballon){
+					var pos = new google.maps.LatLng(ballon.lat, ballon.lng);
+					var marker = new google.maps.Marker({
+						position: pos,
+						map: map,
+						title: ballon.title
+					});
+				});
+
+				// add radar
+				data.radars.forEach(function(radar){
+					var pos = new google.maps.LatLng(radar.lat, radar.lng);
+					var circle = new google.maps.Circle({
+						position: pos,
+						map: map,
+						radius: radar.radius
+					});
+				})
+
+				$('#name').text(data.lost.name);
+				$('#city').text(data.lost.city.name);
+				$('#age').text(data.lost.age);
+			}
+		})
+	};
+
+	google.maps.event.addDomListener(window, 'load', initialize);
 </script>
