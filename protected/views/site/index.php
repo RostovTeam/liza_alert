e9cbbbe560d4ee2c929775a2e3ca879197e3b2d3
Denis Mosolov <denismosolov@gmail.com>
2013-10-12 07:42:18 +0400

    Added map to widget

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -1,9 +1,9 @@
-<div id="map-area"></div>
+<div id="map-area" data-editable="0"></div>
 <div id="name"></div>
 <div id="city"></div>
 <div id="age"></div>
-<div id="coordinator-phone"></div>
 
+<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false&callback=initialize"></script>
 <script type="text/javascript" src="http://yandex.st/jquery/1.10.2/jquery.min.js"></script>
 <script type="text/javascript" src="../static/js/widget.js"></script>
 <script type="text/javascript" src="../static/js/widgetviewmodel.js"></script>
--- static/js/widgetviewmodel.js
+++ static/js/widgetviewmodel.js
@@ -5,24 +5,46 @@ var la = la || {};
 	la.WidgetViewModelDefaults = {
 		name: '#name',
 		city: '#city',
-		age: '#age',
-		phone: '#coordinator-phone'
+		age: '#age'
 	};
 	la.WidgetViewModel = function(data, options) {
 		var wa = this;
 		wa.options = $.extend({}, la.WidgetViewModelDefaults, options);
 		wa.data = data;
-
+		wa.map = undefined;
 		wa.init();
 	};
 
 	la.WidgetViewModel.prototype = {
 		init: function() {
-			wa = this;
-			$(wa.options.name).text(wa.data.name);
-			$(wa.options.city).text(wa.data.city);
-			$(wa.options.age).text(wa.data.age);
-			$(wa.options.phone).text(wa.data.phone);
+			wa = this,
+				lost = wa.data.content.lost;
+			$(wa.options.name).text(lost.name);
+			$(wa.options.city).text(lost.city.name);
+			$(wa.options.age).text(lost.age);
+
+			we.initMap(lost);
+		},
+		initMap: function(lost) {
+			var wa = this,
+				mapOptions = {
+					zoom: 12,
+					disableDefaultUI: true,
+					mapTypeControl: true,
+					zoomControl: true,
+					scaleControl: true,
+					mapTypeId: google.maps.MapTypeId.ROADMAP
+				},
+				map = new google.maps.Map(mapCanvas, mapOptions),
+				geocoder = new google.maps.Geocoder(),
+				address = lost.city.name,
+				editable = Boolean(mapCanvas.getAttribute('data-editable'));
+
+				geocoder.geocode( { 'address': address}, function(results, status) {
+					if (status == google.maps.GeocoderStatus.OK) {
+						map.setCenter(results[0].geometry.location);
+					}
+				});
 		}
 	};
 })(jQuery);
