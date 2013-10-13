adc6cfa72c9396307427936569404bab902de589
komov.r <komov.r@gmail.com>
2013-10-13 13:34:55 +0400

    los cart

--- protected/views/site/frame.php
+++ protected/views/site/frame.php
@@ -5,7 +5,30 @@
 
 
 <div class="span12">
-    <div class="span8">
+    <div class="span8" style="position:relative;">
+        <div id="lost_cart" style="position:absolute;right:25px;bottom:25px;z-index:1;
+             width:260px;heigth:100px;background: rgba(255,255,255,0.7);border-radius: 3px;padding:10px">
+            <div  style="width:100%;text-align:center;
+                  color:red;text-transform: uppercase;margin-bottom: 3px">Внимание! Пропал человек</div>
+            <div   style="float:left;width:80px">
+                <div id ="lost_photo" style="width:100px;height:75px;padding:0px;margin:0px;text-align: center;">
+                    <img src="http://146.185.145.71/static/photos/1381629561_75x75.jpg" />
+                </div>
+            </div>
+
+            <div  style="float:right;width:150px;height:75px;">
+                <div id ="lost_name" style="text-align: center; font-weight:bold;
+                     font-size:16px;text-overflow: ellipsis"> имя</div>
+                <div id="lost_description" style="padding:3px;font-size:12px;text-overflow: ellipsis
+                     ">трала ыва ыва ыв ыва</div>
+            </div>
+
+            <div  id ="lost_age" style="float:left; width:100px;text-align: center;margin-top:3px;font-weight: bold"> 1995</div>
+            <div  id ="lost_forum_link" style="float:right; 
+                  width:130px;text-align:right;padding-right:15px;margin-top:3px;">link</div>
+            <div></div>
+        </div>
+
         <div id="map-canvas" data-editable="<?= $editable ?>" data-lost-id="<?= $lost_id ?>"></div>
     </div>
     <? if ($editable): ?>
