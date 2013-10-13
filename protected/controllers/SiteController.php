b7d04f19519e4dec397e6f970579694fd346f50e
kostyukdg <kostyukdg@gmail.com>
2013-10-13 08:03:50 +0400

    redirect to lizaalert

--- protected/controllers/SiteController.php
+++ protected/controllers/SiteController.php
@@ -8,15 +8,9 @@
 class SiteController extends Controller
 {
 
-    function actionIndex($id)
+    function actionIndex()
     {
-        header('Access-Control-Allow-Origin: *');
-        header("Access-Control-Allow-Methods: POST, GET");
-        header("Access-Control-Allow-Headers: x-requested-with");
-        if (Lost::model()->findByPk($id))
-            $this->render('index', array('id' => $id));
-        else
-            $this->render('index', array('id' => null));
+        $this->redirect('http://lizaalert.org');
     }
 
     function actionTest()
