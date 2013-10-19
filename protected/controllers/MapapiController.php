a4e6814551a124cbaf1f128a15199849d15a0fce
komov.r <komov.r@gmail.com>
2013-10-19 23:36:50 +0400

    Issue #13 add map center lat/lng and zoom

--- protected/controllers/MapapiController.php
+++ protected/controllers/MapapiController.php
@@ -6,9 +6,14 @@ class MapapiController extends ApiController
     public function accessRules()
     {
         return array_merge(
-                array(array('allow',
-                'actions' => array('view', 'create'),
-               'roles'=>array('superuser','admin')
+                array(
+            array('allow',
+                'actions' => array('view'),
+                'users' => array('*')
+            ),
+            array('allow',
+                'actions' => array('create'),
+                'roles' => array('superuser', 'admin')
             )
                 ), parent::accessRules()
         );
@@ -56,6 +61,26 @@ class MapapiController extends ApiController
             $this->_sendResponse(400, array('error' => 'No lost_id'));
         }
 
+        $lost = Lost::model()->findByPk($_POST['lost_id']);
+
+        if (!$lost)
+        {
+            $this->_sendResponse(400, array('error' => 'Lost not found'));
+        }
+
+        if (isset($_POST['map_lat']) && isset($_POST['map_lng']))
+        {
+            $lost->map_lat = $_POST['map_lat'];
+            $lost->map_lng = $_POST['map_lng'];
+        }
+
+        if (isset($_POST['map_zoom']))
+        {
+            $lost->map_zoom = $_POST['map_zoom'];
+        }
+
+        $lost->save();
+
         if (!isset($_REQUEST['Balloon']) && !isset($_REQUEST['Radar']) && !isset($_REQUEST['Area']))
         {
             $this->_sendResponse(400, array('error' => 'Nothing to save'));
@@ -72,7 +97,7 @@ class MapapiController extends ApiController
             {
                 $modelname::model()->deleteAllByAttributes(array('lost_id' => $_POST['lost_id']));
             }
-            
+
             if (is_array($_REQUEST[$postindex])) //&& intval($_REQUEST[$postindex][0]['lost_id']))
             {
                 foreach ($_REQUEST[$postindex] as $key => $data)
